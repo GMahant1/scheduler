987e0a8212c3d2c44a18b52c2d97104649e1751e
Gaurav Mahant <thegau22@gmail.com>
2023-02-02 22:34:40 -0700

    create DayList component to generate each DayListItem

---
+++ src/components/DayList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList (props) {
+
+  const schedule = props.days.map(dayArr => {
+    return (
+      <DayListItem
+        key={dayArr.id}
+        name={dayArr.name}
+        spots={dayArr.spots}
+        selected={dayArr.name === props.value}
+        setDay={props.onChange} />
+    )
+  })
+
+  return <ul>{schedule}</ul>;
+}
