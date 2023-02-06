dec81b1d86533397a10b4fa9c1d9c6cbadfb4f24
Gaurav Mahant <thegau22@gmail.com>
2023-02-05 21:06:48 -0700

    update props

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,16 +1,17 @@
 import React from "react";
-import DayListItem from "./DayListItem";
+import DayListItem from "components/DayListItem";
 
 export default function DayList (props) {
 
   const schedule = props.days.map(dayArr => {
+    
     return (
       <DayListItem
         key={dayArr.id}
         name={dayArr.name}
         spots={dayArr.spots}
         selected={dayArr.name === props.value}
-        setDay={props.onChange} />
+        setDay={props.setDay} />
     )
   })
 
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,30 +3,27 @@ import "components/DayListItem.scss";
 import classNames from "classnames";
 
 const formatSpots = function (spots) {
-
   if (!spots) {
     return "no spots remaining";
   }
-
   if (spots === 1) {
     return `${spots} spot remaining`;
   }
-
   return `${spots} spots remaining`;
 };
 
 export default function DayListItem(props) {
 
-  const dayClass = classNames("day-list__item", 
+  const availabiliy = formatSpots(props.spots);
+
+  let dayClass = classNames("day-list__item", 
   { "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots 
   })
 
-  const availabiliy = formatSpots(props.spots);
-
   return (
     <li className={dayClass}
-    onClick={() => props.setDay(props.name)}>
+        onClick={() => props.setDay(props.name)}>
 
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{availabiliy}</h3>
