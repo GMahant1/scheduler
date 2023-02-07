32fd0b5ac49f150b85435e05b7940a969d399875
Gaurav Mahant <thegau22@gmail.com>
2023-02-07 16:52:38 -0700

    add storybook information for interviewerListItem

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import DayListItem from "components/DayListItem";
 
 import DayList from "components/DayList";
 
+import InterviewerListItem from "components/InterviewerListItem";
+
 
 //Button
 storiesOf("Button", module)
@@ -73,3 +75,39 @@ storiesOf("Button", module)
     .add("Wednesday", () => (
         <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
     ));
+
+//interviwerListItem
+
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={action("setInterviewer")}
+    />
+  ));
