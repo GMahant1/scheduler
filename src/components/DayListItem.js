7dcee1da81098e4a0b7652ac686897001557708c
Gaurav Mahant <thegau22@gmail.com>
2023-02-07 17:56:45 -0700

    update prop names passed down from DayList component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -22,9 +22,7 @@ export default function DayListItem(props) {
   })
 
   return (
-    <li className={dayClass}
-        onClick={() => props.setDay(props.name)}>
-
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{availabiliy}</h3>
     </li>
