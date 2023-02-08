39ae8695f538bfabee7d0292b8c12116c6cdd7f3
Gaurav Mahant <thegau22@gmail.com>
2023-02-07 17:35:53 -0700

    update onClick to run with predefined function passed down as a prop

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -8,7 +8,7 @@ export default function InterviewerListItem(props) {
   {"interviewers__item--selected": props.selected});
 
   return (
-    <li className={interviewerClass} onClick={() => {props.setInterviewer(props.id)}}>
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
