175bfbafb69fd3938db201cfe7e0ee52660f4ca4
Gaurav Mahant <thegau22@gmail.com>
2023-02-09 20:46:17 -0700

    create story for Empty.js and Show.js

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 //Button
 
@@ -158,6 +159,11 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time = "12pm" />)  
+  .add("Appointment with Time", () => <Appointment time= "12pm" />)  
   .add("Header", () => <Header time= "12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show  
+                        student="Lydia Miller-Jones" 
+                        interviewer={interviewers[0]} 
+                        onEdit={action("onEdit")} 
+                        onDelete={action("onDelete")} />);
