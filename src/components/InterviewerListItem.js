39454675d81c18bc90a51ace3c3b281deea8f825
Gaurav Mahant <thegau22@gmail.com>
2023-02-10 15:50:26 -0700

    update prop passed in on click

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -8,7 +8,7 @@ export default function InterviewerListItem(props) {
   {"interviewers__item--selected": props.selected});
 
   return (
-    <li className={interviewerClass} onClick={props.setInterviewer}>
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.name)}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
