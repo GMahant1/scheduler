b0667128626cf0b495e32aab447c9a3a13843f16
Gaurav Mahant <thegau22@gmail.com>
2023-02-22 08:44:42 +0530

    define interviewers and onCancel props

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,17 +3,40 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "hooks/useVisualMode"; 
+import Form from "components/Appointment/Form";
 
 
 export default function Appointment (props) {
 
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  //mode constants for visualMode function
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  
+
   return (
-  <article className="appointment">
-    <Header time={props.time} />
-      {props.interview ? 
-        <Show 
+    <article className="appointment">
+      <Header time={props.time} />
+      {mode === SHOW && (
+        <Show
           student={props.interview.student}
-          interviewer={props.interview.interviewer} />
-        : <Empty /> }
-  </article>);
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          // onSave={onSave}
+          onCancel={() => back()}
+        />
+      )}
+    </article>);
 };
+
