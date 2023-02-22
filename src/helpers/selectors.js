9fff9d89d64776a1328729cd639b6aa4adacb22f
Gaurav Mahant <thegau22@gmail.com>
2023-02-22 17:54:48 +0530

    create getInterviewersForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,11 +1,11 @@
-export function getAppointmentsForDay (state, days) {
+export function getAppointmentsForDay(state, days) {
   const appointments = [];
 
   for (const day of state.days) {
     if (day.name === days) {
       for (const app of day.appointments) {
         if (state.appointments[app.toString()]) {
-          appointments.push(state.appointments[app])
+          appointments.push(state.appointments[app]);
         }
       }
     }
@@ -13,14 +13,30 @@ export function getAppointmentsForDay (state, days) {
   return appointments;
 }
 
-export function getInterview (state, interview) {
+export function getInterviewersForDay(state, days) {
+
+  const interviewers = [];
+
+  for (const day of state.days) {
+    if (day.name === days) {
+      for (const interviewer of day.interviewers) {
+        if (state.interviewers[interviewer]) {
+          interviewers.push(state.interviewers[interviewer]);
+        }
+      }
+    }
+  }
+  return interviewers;
+}
+
+export function getInterview(state, interview) {
 
   if (!interview) {
-   return null;
- }
- const interviewer = state.interviewers[interview.interviewer];
- if (!interviewer) {
-   return null;
- }
- return { ...interview, interviewer};
+    return null;
+  }
+  const interviewer = state.interviewers[interview.interviewer];
+  if (!interviewer) {
+    return null;
+  }
+  return { ...interview, interviewer };
 }
