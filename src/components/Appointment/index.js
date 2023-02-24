4fe05218141c7c6c827b74a811a70b54cbb1d20b
Gaurav Mahant <thegau22@gmail.com>
2023-02-24 11:02:22 +0530

    define onSave function to update the form component and show saving status message

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,22 +3,34 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
-import useVisualMode from "hooks/useVisualMode"; 
+import useVisualMode from "hooks/useVisualMode";
 import Form from "components/Appointment/Form";
+import Status from "components/Appointment/Status";
 
 
-export default function Appointment (props) {
+export default function Appointment(props) {
 
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
 
   //mode constants for visualMode function
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
-  
+
+  function onSave(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING);
+    props.bookInterview(props.id, interview)
+    .then(() => transition(SHOW))
+  }
+
 
   return (
     <article className="appointment">
@@ -32,11 +44,12 @@ export default function Appointment (props) {
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === CREATE && (
         <Form
-          interviewers={[]}
-          // onSave={onSave}
+          interviewers={props.interviewers}
+          onSave={onSave}
           onCancel={() => back()}
         />
       )}
+       {mode === SAVING && (<Status message={`Saving...`}/> )}
     </article>);
 };
 
