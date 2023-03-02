b0d2ccfe279683a6e3f9cde2c2e615c52c1fee4b
Gaurav Mahant <thegau22@gmail.com>
2023-03-02 13:51:52 -0700

    introduce an error to trigger prop-types, resolve after

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
+import PropTypes from "prop-types";
 
 export default function InterviewerList (props) {
 
@@ -19,5 +20,10 @@ export default function InterviewerList (props) {
   return <section className="interviewers">
   <h4 className="interviewers__header text--light">Interviewer</h4>
   <ul className="interviewers__list">{interviewerSchedule}</ul>
-</section>
+  </section>
+
+};
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
 };
