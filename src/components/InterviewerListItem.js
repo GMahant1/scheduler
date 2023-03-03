0d7d40b5ec65d2f825d9cdce06e5200135c280a9
Gaurav Mahant <thegau22@gmail.com>
2023-03-03 12:10:59 -0700

    change hardcoded ALT to prop passed down

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -12,7 +12,7 @@ export default function InterviewerListItem(props) {
       <img
         className="interviewers__item-image"
         src={props.avatar}
-        alt="Sylvia Palmer"
+        alt={props.name}
       />
       {props.selected && props.name}
     </li>);
